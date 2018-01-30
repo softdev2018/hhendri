f22506c288782a01de970d3709d25c3e99827da4
haidarhendri <haidarhendri7@gmail.com>
2018-01-30 15:41:40 +0700

    Hari Ke-2
    Membuka ruang LAB KOMP 2, Review materi yang telah dipelajari hari kemarin, Mempelajari CSS lebih lanjut di Sololearn, Mempelajari Javascript di Sololearn, Mempelajari HTML, CSS, dan Javascript melalui https://www.cssinhtml.com, Membuat gambaran tentang project pekan ini, yaitu membuat sebuah web blog, Mempelajari pengenalan PHP secara umum, Mempelajari cara menerima telepon dari pihak luar ke UPT TIK

---
+++ Hari Ke-2/forLoop.js
@@ -0,0 +1,11 @@
+for (i=1; i<=5; i++) {
+    document.write(i + "<br />");
+}
+var i = 1;
+for (; i<=5; i++) {
+   document.write(i + "<br />");
+}
+for (i=1, text=""; i<=5; i++) {
+   text = i;
+   document.write(i + "<br />");
+}
